31b9dcaa19920d207af8659c57dedce84f78b4c4
Mex777 <fologeaalexandru@gmail.com>
2022-10-15 10:50:15 +0300

    added login component

---
+++ src/Components/login-page/Login.js
@@ -0,0 +1,73 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const fetchForm = async (username, password) => {
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+
+  const urlencoded = new URLSearchParams();
+  urlencoded.append("username", username);
+  urlencoded.append("password", password);
+
+  const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: urlencoded,
+    redirect: "follow",
+  };
+
+  const res = await fetch(
+    `${process.env.REACT_APP_API_URL}/login`,
+    requestOptions
+  );
+  const resJson = await res.json();
+
+  return resJson;
+};
+
+function Login() {
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+  const [error, setError] = useState();
+  const navigate = useNavigate();
+
+  const submit = async (e) => {
+    e.preventDefault();
+    const res = await fetchForm(username, password);
+    console.log(res);
+
+    if (res.token) {
+      localStorage.setItem("secret_token", res.token);
+      navigate("/");
+      return;
+    }
+
+    setError("Username or password incorrect");
+  };
+
+  return (
+    <div>
+      <form>
+        <label>Username</label>
+        <input
+          name="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+        <label>Password</label>
+        <input
+          name="password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <button onClick={submit}>Submit</button>
+      </form>
+      <p>{error}</p>
+    </div>
+  );
+}
+
+export default Login;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import App from "./Components/index-page/App";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./Components/login-page/Login";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/" element={<App />}></Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
